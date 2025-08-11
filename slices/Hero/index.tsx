c34b84719f671d704385b66b8122b5c57c97dce0
Thuc29 <112091905+Thuc29@users.noreply.github.com>
2025-08-11 12:38:08 +0700

    create Hero slice and helper components

--- slices/Hero/index.tsx
+++ slices/Hero/index.tsx
@@ -1,12 +1,22 @@
 import { FC } from "react";
 import { Content } from "@prismicio/client";
-import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import { JSXMapSerializer, PrismicRichText, SliceComponentProps } from "@prismicio/react";
 import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
 import Bounded from "@/component/Bounded";
 import Button from "../../component/Button";
-/**
- * Props for `Hero`.
- */
+import Heading from "../../component/Heading";
+
+
+const components: JSXMapSerializer = {
+  heading1: ({ children }) => (
+    <Heading as="h1" size="xl" className="md:mb-8 mb-4 mt-12 first:mt-0 last:mb-0" >{children}</Heading>
+  ),
+  paragraph: ({ children }) => (
+    <p className="md:mb-8 max-w-md text-2xl text-center font-normal mb-4 leading-10 text-slate-600 font-sans">{children}</p>
+  )
+}
+
+
 export type HeroProps = SliceComponentProps<Content.HeroSlice>;
 
 /**
@@ -20,17 +30,8 @@ const Hero: FC<HeroProps> = ({ slice }) => {
     >
       <div className="grid grid-cols-1 place-items-center text-center">
 
-        <PrismicRichText field={slice.primary.heading} components={{
-          heading1: ({ children }) => (
-            <h1 className="md:text-7xl text-5xl font-bold leading-tight tracking-tight font-nunito text-slate-700
-          ">{children}</h1>
-          )
-        }} />
-        <PrismicRichText field={slice.primary.body} components={{
-          paragraph: ({ children }) => (
-            <p className="md:mb-8 max-w-md text-2xl text-center font-normal mb-4 leading-10 text-slate-600 font-sans">{children}</p>
-          )
-        }} />
+        <PrismicRichText field={slice.primary.heading} components={components} />
+        <PrismicRichText field={slice.primary.body} components={components} />
         <Button field={slice.primary.button_link} className="mb-8 md:mb-10">
           {slice.primary.button_text}
         </Button>
