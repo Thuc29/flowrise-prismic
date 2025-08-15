62ceca207ee73460a0a91e66e063fef5376257c5
Thuc29 <112091905+Thuc29@users.noreply.github.com>
2025-08-15 13:37:52 +0700

    Add testininals

--- slices/Testimonial/index.tsx
+++ slices/Testimonial/index.tsx
@@ -1,53 +1,94 @@
-import { FC } from "react";
-import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import { Content, isFilled } from "@prismicio/client";
+import {
+  SliceComponentProps,
+  JSXMapSerializer,
+  PrismicRichText,
+} from "@prismicio/react";
+
+
+import { createClient } from "@/prismicio";
+import { PrismicNextImage } from "@prismicio/next";
+import Heading from "../../component/Heading";
+import Bounded from "../../component/Bounded";
+
+const components: JSXMapSerializer = {
+  heading2: ({ children }) => (
+    <Heading as="h2" size="md" className="text-center mb-9 font-semibold font-nunito">
+      {children}
+    </Heading>
+  ),
+  paragraph: ({ children }) => (
+    <p className="text-xl md:text-2xl font-sans font-normal font-body text-slate-600 mb-8">
+      {children}
+    </p>
+  ),
+};
 
 /**
- * Props for `Testimonial`.
+ * Props for `Testimonials`.
  */
-export type TestimonialProps = SliceComponentProps<Content.TestimonialSlice>;
+export type TestimonialsProps = SliceComponentProps<Content.TestimonialSlice>;
 
 /**
- * Component for "Testimonial" Slices.
+ * Component for "Testimonials" Slices.
  */
-const Testimonial: FC<TestimonialProps> = ({ slice }) => {
+const Testimonials: React.FC<TestimonialsProps> = async ({ slice }) => {
+  // Create a Prismic client to fetch data
+  const client = createClient();
+
+  const testimonials = await Promise.all(
+    slice.primary.testimonial.map((item) => {
+      if (
+        isFilled.contentRelationship(item.testimonial) &&
+        item.testimonial.uid
+      ) {
+        return client.getByUID("testimonial", item.testimonial.uid);
+      }
+    })
+  );
+  console.log("Testimonials data:", testimonials);
+
   return (
-    <section
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      Placeholder component for testimonial (variation: {slice.variation})
-      slices.
-      <br />
-      <strong>You can edit this slice directly in your code editor.</strong>
-      {/**
-       * 💡 Use Prismic MCP with your code editor
-       *
-       * Get AI-powered help to build your slice components — based on your actual model.
-       *
-       * ▶️ Setup:
-       * 1. Add a new MCP Server in your code editor:
-       *
-       * {
-       *   "mcpServers": {
-       *     "Prismic MCP": {
-       *       "command": "npx",
-       *       "args": ["-y", "@prismicio/mcp-server@latest"]
-       *     }
-       *   }
-       * }
-       *
-       * 2. Select a model optimized for coding (e.g. Claude 3.7 Sonnet or similar)
-       *
-       * ✅ Then open your slice file and ask your code editor:
-       *    "Code this slice"
-       *
-       * Your code editor reads your slice model and helps you code faster ⚡
-       * 🎙️ Give your feedback: https://community.prismic.io/t/help-us-shape-the-future-of-slice-creation/19505
-       * 📚 Documentation: https://prismic.io/docs/ai#code-with-prismics-mcp-server
-       */}
-    </section>
+      <PrismicRichText components={components} field={slice.primary.heading} />
+      <div className="grid lg:grid-cols-3 grid-cols-1 gap-8">
+        {testimonials.map(
+          (item, index) =>
+            item && (
+              <div
+                key={index}
+                className="border bg-white shadow-lg rounded-lg px-8 md:px-14 py-10 md:py-16 grid content-between"
+              >
+                <blockquote className="text-xl md:text-2xl font-normal font-nunito text-slate-600 mb-8">
+                  {item.data.quote}
+                </blockquote>
+
+                <div className="flex items-center font-sans">
+                  <PrismicNextImage
+                    width={56}
+                    height={56}
+                    field={item.data.avatar}
+                    className="rounded-full mr-4 aspect-square"
+                    imgixParams={{ ar: "1:1", fit: "crop" }}
+                  />
+                  <div>
+                    <p className="text-base font-medium text-slate-700">
+                      {item.data.name}
+                    </p>
+                    <p className="text-base text-slate-600">
+                      {item.data.job_title}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            )
+        )}
+      </div>
+    </Bounded>
   );
 };
 
-export default Testimonial;
+export default Testimonials;
